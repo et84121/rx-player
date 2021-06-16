e655b6e157f0ae8edcf3c2891e5f853833b1b553
Paul Berberian <pea.berberian@gmail.com>
2021-06-16 15:15:15 +0200

    remove defaultAudioTrack and defaultTextTrack options as well as corresponding IDefaultAudioTrackOption and IDefaultTextTrackOption types

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -40,8 +40,6 @@ export {
   IKeySystemOption,
   ISupplementaryTextTrackOption,
   ISupplementaryImageTrackOption,
-  IDefaultAudioTrackOption,
-  IDefaultTextTrackOption,
   INetworkConfigOption,
   IStartAtOption,
 } from "./option_utils";
