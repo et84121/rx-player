478646c3aeaeba2befc835dbe8f6447fc14e142f
Paul Berberian <paul.berberian@proton.me>
2025-03-19 17:40:18 +0100

    tests: check in globalSetup that the server is only started once

--- tests/globalSetup.mjs
+++ tests/globalSetup.mjs
@@ -1,14 +1,18 @@
 import createContentServer from "./contents/server.mjs";
 
-const realConsoleWarn = console.warn;
 let contentServer;
+
+let started = false;
 
 /**
  * Peform actions we want to setup before tests.
  */
 export function setup() {
-  removeAnnoyingDeprecationNotice();
+  if (started) {
+    return; // already started
+  }
   contentServer = createContentServer();
+  started = true;
 }
 
 /**
@@ -16,28 +20,5 @@ export function setup() {
  */
 export function teardown() {
   contentServer?.close();
-}
-
-/**
- * Webdriverio just spams a deprecation notice with the current version of
- * vitest (one per test, though as we have thousands of tests, it just fills the
- * output into something unreadable).
- *
- * This is so annoying that I just chose to mute it by monkey-patching the
- * console function here.
- *
- * This should hopefully be very temporary.
- */
-function removeAnnoyingDeprecationNotice() {
-  console.warn = function (...args) {
-    if (
-      typeof args[0] === "string" &&
-      args[0].startsWith(
-        '⚠️ [WEBDRIVERIO DEPRECATION NOTICE] The "switchToFrame" command is deprecated and we encourage everyone to use `switchFrame` instead for switching into frames.',
-      )
-    ) {
-      return;
-    }
-    return realConsoleWarn.apply(console, args);
-  };
+  started = false;
 }
